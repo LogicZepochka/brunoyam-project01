f3d90551f14f8e503eb70766ebe4b5370189cb2d
LogZep <logzep@gmal.com>
2025-08-01 20:43:35 +0500

    (ref): Добавлены try-catch в registration.service

--- backend/src/services/registration.service.ts
+++ backend/src/services/registration.service.ts
@@ -1,34 +1,66 @@
 import { ApiError } from "../builders/api/errors.enum";
 import { RegistrationDataDTO } from "../dto/RegistrationDTO";
+import CreateLogger, { LogLevel } from "../etc/logger";
 import { UserRepository } from "../repositories/user.reposiotory.interface";
 import * as argon2 from "argon2"
 
 
-export default class RegistrationService {
+export default class RegistrationService implements Disposable {
 
     private repo: UserRepository
+    private log = CreateLogger("RegistrationService")
 
     constructor(userRepository: UserRepository) {
         this.repo = userRepository
+        this.log("Created RegistrationService instance",LogLevel.Debug)
     }
 
-    async RegisterUser(data: RegistrationDataDTO): Promise<boolean> {
-        let result = this.repo.createUser({
-            ...data,
-            password: await argon2.hash(data.password)
-        });
-        return result !== null;
+    [Symbol.dispose](): void {
+        this.repo.end();
+        this.log("Destroyed RegistrationService instance",LogLevel.Debug)
     }
 
-    async IsEmailFree(email: string): Promise<boolean> {
-        let result = await this.repo.findBy({email: email})
-        return result === null;
-    }
+        async RegisterUser(data: RegistrationDataDTO): Promise<boolean> {
+            try {
+                let result = this.repo.createUser({
+                    ...data,
+                    password: await argon2.hash(data.password)
+                });
+                this.log(`New user registered: ${result !== null}`,LogLevel.Debug)
+                return result !== null;
+            }
+            catch(e) {
+                this.log(`Failed to register user:`,LogLevel.Error)
+                this.log(`${e}`,LogLevel.Error)
+                return false;
+            }
+        }
 
-    async isPhoneFree(phone: string): Promise<boolean> {
-        let result = await this.repo.findBy({phone: phone})
-        return result === null;
-    }
+        async IsEmailFree(email: string): Promise<boolean> {
+            try {
+                let result = await this.repo.findBy({email: email})
+                return result === null;
+            }
+            catch(e) {
+                this.log(`Failed to search user with email:`,LogLevel.Error)
+                this.log(`${e}`,LogLevel.Error)
+                throw Error("Failed to search user email: "+e)
+            }
+            
+        }
+
+        async isPhoneFree(phone: string): Promise<boolean> {
+            try {
+                let result = await this.repo.findBy({phone: phone})
+            return result === null;
+            }
+            catch(e) {
+                this.log(`Failed to search user with phone:`,LogLevel.Error)
+                this.log(`${e}`,LogLevel.Error)
+                throw Error("Failed to search user phone: "+e)
+            }
+            
+        }
 
 
 }
