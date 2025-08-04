71ef33d179a196602948f17507ad8b669ad58bdf
LogZep <logzep@gmal.com>
2025-08-04 23:38:26 +0500

    (feat): Создан сервис для комнат

---
+++ backend/src/services/room.service.ts
@@ -0,0 +1,50 @@
+import CreateLogger, { LogLevel } from "../etc/logger";
+import { RoomRepository } from "../repositories/room.repository.interface";
+import { Room, User } from "../repositories/types";
+
+
+
+export default class RoomService {
+
+    private repo: RoomRepository
+    private logger = CreateLogger("RoomService")
+
+    constructor(userRepository: RoomRepository) {
+        this.repo = userRepository
+    }
+
+    async getRoom(_id: string) {
+        try {
+            let result = await this.repo.getRoom(_id);
+            return result;
+        }
+        catch(e) {
+            return null;
+        }
+    }
+
+    async createUser(user: Room,ownerId: string): Promise<Room | null> {
+        try {
+            let result = await this.repo.createRoom(user,ownerId);
+            this.logger(`Created new room ${result?.title} with id ${result?._id} (Owner: ${ownerId})`,LogLevel.Debug)
+            return result;
+        }
+        catch(e) {
+            this.logger(`Failed to create new room:\n${e}`,LogLevel.Error)
+            return null;
+        }
+    }
+
+
+    async updateUser(id: string,data: Partial<User>): Promise<boolean> {
+        try {
+            await this.repo.updateRoom(id,data);
+            this.logger(`Update room with id ${id}`,LogLevel.Debug)
+            return true;
+        }
+        catch(e) {
+            this.logger(`Failed to update room:\n${e}`,LogLevel.Error)
+            return false;
+        }
+    }
+}
