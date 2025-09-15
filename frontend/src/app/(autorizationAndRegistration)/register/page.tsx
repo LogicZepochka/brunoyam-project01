4abe29ee63bcc4d5deb13a2088b7c6baed84886e
LogZep <logzep@gmal.com>
2025-09-15 17:20:09 +0500

    (feat): Добавлены названия страниц

--- frontend/src/app/(autorizationAndRegistration)/register/page.tsx
+++ frontend/src/app/(autorizationAndRegistration)/register/page.tsx
@@ -2,6 +2,11 @@ import SignInForm from "@/components/signin/SignInForm";
 import SignUpForm from "@/components/signin/SignUpForm";
 import { Box, Container, Grid, Paper } from "@mui/material";
 
+export const metadata = {
+  title: "Dwellio - Регистрация",
+  description: "Dwellio — это удобный сервис для поиска, бронирования и аренды жилья. Найдите идеальное место для проживания быстро и просто.",
+};
+
 export default function Home() {
   return (
     <Container maxWidth="xl">
--- frontend/src/app/(autorizationAndRegistration)/signin/page.tsx
+++ frontend/src/app/(autorizationAndRegistration)/signin/page.tsx
@@ -1,7 +1,12 @@
 import SignInForm from "@/components/signin/SignInForm";
 import { Box, Container, Grid, Paper } from "@mui/material";
 
-export default function Home() {
+export const metadata = {
+  title: "Dwellio - Авторизация",
+  description: "Dwellio — это удобный сервис для поиска, бронирования и аренды жилья. Найдите идеальное место для проживания быстро и просто.",
+};
+
+export default function SignInPage() {
   return (
     <Container maxWidth="xl">
       <Grid
